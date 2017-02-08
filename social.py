8ff58776567aba6363b55e2a342c1c00c90df011
Randy M. Roberts <rsqrd@lanl.gov>
2017-02-08 15:27:37 -0700

    o -- added doc strings

--- social.py
+++ social.py
@@ -1,30 +1,58 @@
+""" Module to solve social network problems.
+
+Reference: Computing global structural balance in large-scale signed social networks, by Giuseppe Facchetti, et. al.
+"""
+
 import dwave_sapi2.util as util
 import dwave_sapi2.embedding as embedding
 import dwave_sapi2.core as core
 
 class Network(object):
+    """ Network is used to setup the social network as described in Facchetti, et. al.
+    """
     def __init__(self):
+        # maxNode is the highest referenced node number.  Nodes assumed to be numbered from 0 to maxNode.
         self.maxNode = 0
+        # j is the dictionary of edge weights, with (node1, node2) being the key to the dictionary
         self.j = {}
 
     def friend(self, n1, n2):
+        """ Set the edge from nodes n1 to n2 to be a friend. """
         self.maxNode = max(self.maxNode, n1, n2)
+        # using opposite sign convention from Facchetti, et.al. since they minimize -s^Js instead of s^Js
         self.j[(n1,n2)] = -1
 
     def enemy(self, n1, n2):
+        """ Set the edge from nodes n1 to n2 to be an enemy. """
         self.maxNode = max(self.maxNode, n1, n2)
+        # using opposite sign convention from Facchetti, et.al. since they minimize -s^Js instead of s^Js
         self.j[(n1,n2)] = 1
 
     def J(self):
+        """ Return the edge weights of the social network. """
         return self.j
 
     def solve(self, solver, s=0.5, discard=True, verbose=0, **kws):
+        """ solve the social network as an Ising model
+
+        Args:
+            solver: D-Wave solver object.
+            s: scale the network weights by s before adding chain weights.
+            discard: True to ignore solutions that violate chain constraints
+            verbose: 0 or 1
+            **kws: the other keywords are passed to the find_embedding method
+        """
+
         kws['verbose'] = verbose
+
+        # have D-Wave find an embedding of our network into the Chimera
         A = util.get_hardware_adjacency(solver)
         emb = embedding.find_embedding(self.j, A, **kws)
         if verbose:
             print 'embedding:', emb
 
+        # use the new embedding to set up a new Ising model that we will
+        # actually send to the solver
         h = []
         (h0, j0, jc, new_emb) = embedding.embed_problem(h, self.j, emb, A)
         if verbose:
@@ -33,23 +61,34 @@ class Network(object):
             print "jc:", jc
             print "new embedding:", new_emb
 
-        emb_j = {t: s*j0[t] for t in j0}
-        emb_j.update(jc)
+        # scale the network edge weights, j0,  by s before adding the chain weights, jc.
+        jEmbeding = {t: s*j0[t] for t in j0}
+        jEmbeding.update(jc)
         if verbose:
-            print "new j (s scaled):", emb_j
+            print "new j (s scaled):", jEmbeding
 
-        ans = core.solve_ising(solver, h0, emb_j, num_reads=1000)
+        # solve the embedded Ising model
+        ans = core.solve_ising(solver, h0, jEmbeding, num_reads=1000)
         if verbose:
             print ans
 
+        # convert the solution back into the original, un-embedded, problem
         if discard:
             res = embedding.unembed_answer(ans['solutions'], new_emb, 'discard', h, self.j)
         else:
             res = embedding.unembed_answer(ans['solutions'], new_emb, 'minimize_energy', h, self.j)
 
+        # pack it up into a solution object that does a bit of post-processing on the solution
         return Solution(ans, res, self.j)
 
 class Solution(object):
+    """ Solution is used to package up and post-process the results from the Ising model calculation.
+
+    It separates the results into a list of solutions, ordered by energy.
+    Each solution is a dictionary with keys 'energy', 'sJs', 'spins', 'num_occ', and 'delta'.
+    The 'delta' term is the global balance of the network as calculated in [2] of Fracchetti, et.al.
+
+    """
     def __init__(self, ans, res, j):
         self.ans = ans
         self.j = j
@@ -65,6 +104,8 @@ class Solution(object):
     def __delta__(self, s, j):
         sjs = self.__sJs__(s, j)
         m = len(j)
+        # the plus sign is due to Fracchetti et.al. minimizing -s^Js, while we change the sign of J and minimize s^Js
+        # also, I think m is defined incorrectly in Fracchetti et.al.
         return .5 * (m + sjs)
 
     def __sJs__(self, s, j):
