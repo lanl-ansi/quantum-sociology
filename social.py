69bd274feca50e79815ab7df56cf151679470790
Randy M. Roberts <rsqrd@lanl.gov>
2017-02-08 11:29:28 -0700

    o -- making progress

--- social.py
+++ social.py
@@ -1,10 +1,61 @@
+import dwave_sapi2.util as util
+import dwave_sapi2.embedding as embedding
+import dwave_sapi2.core as core
+
 class Network(object):
     def __init__(self):
         self.maxNode = 0
         self.j = {}
+
     def friend(self, n1, n2):
         self.maxNode = max(self.maxNode, n1, n2)
-        self.j[(n1,n2)] = 1
+        self.j[(n1,n2)] = -1
+
     def enemy(self, n1, n2):
         self.maxNode = max(self.maxNode, n1, n2)
-        self.j[(n1,n2)] = -1
+        self.j[(n1,n2)] = 1
+
+    def J(self):
+        return self.j
+
+    def solve(self, solver, s=0.5, discard=True, **kws):
+        A = util.get_hardware_adjacency(solver)
+        emb = embedding.find_embedding(self.j, A, **kws)
+        print 'embedding:', emb
+
+        h = []
+        (h0, j0, jc, new_emb) = embedding.embed_problem(h, self.j, emb, A)
+        #print "h0:", h0
+        print "j0:", j0
+        print "jc:", jc
+        print "new embedding:", new_emb
+
+        emb_j = {t: s*j0[t] for t in j0}
+        emb_j.update(jc)
+        print "new j (s scaled):", emb_j
+
+        ans = core.solve_ising(solver, h0, emb_j, num_reads=1000)
+        #print ans
+
+        if discard:
+            res = embedding.unembed_answer(ans['solutions'], new_emb, 'discard', h, self.j)
+        else:
+            res = embedding.unembed_answer(ans['solutions'], new_emb, 'minimize_energy', h, self.j)
+
+        return Solution(ans, res, self.j)
+
+class Solution(object):
+    def __init__(self, ans, res, j):
+        self.ans = ans
+        self.j = j
+        self.res = [ {'energy': e, 'sJs:': self.sJs(s), 'spins': s, 'num_occ': n}
+                     for (e,s,n) in zip(ans['energies'],res, ans['num_occurrences']) ]
+
+    def sJs(self, s):
+        sum = 0
+        for jj in self.j:
+            sum += self.j[jj]*s[jj[0]]*s[jj[1]]
+        return sum
+
+    def results(self):
+        return self.res
--- testSocial.py
+++ testSocial.py
@@ -1,6 +1,26 @@
 import social
+import dwave_sapi2.local as local
+import dwave_sapi2.remote as remote
 
-net = social.Network
+net = social.Network()
 net.enemy(0,1)
 net.friend(0,2)
 net.enemy(1,2)
+
+#print net.__dict__
+
+if True:
+    conn = local.local_connection
+    solver = conn.get_solver("c4-sw_sample")
+else:
+    conn = remote.RemoteConnection('https://localhost:10443/sapi', 'LANL-2690b414a0be9fc9af1d82d00bfe1ef23936c99e')
+    solver = conn.get_solver("DW2X")
+
+res = net.solve(solver, discard=True, verbose=0)
+
+print
+print 'J:', net.J()
+print
+print 'results:'
+for r in res.results():
+    print r
