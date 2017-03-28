9ce2e8ee368f35a5f8b7a15815d32ac5f991ca58
Randy M. Roberts <rsqrd@lanl.gov>
2017-03-28 09:52:23 -0600

    o -- major refactoring and cleanup

--- runSocialSolves.py
+++ runSocialSolves.py
@@ -7,108 +7,139 @@ import social
 import terrorGraphs as tg
 import plotTerrorGraphs as ptg
 
-use_dwave = True
-save_plot = True
+use_dwave = False
+save_plot = False
 
-def solve(net, emb, verbose=0, print_res=False):
-    print
-    print 'net.j:', net.j()
+class StanfordDwave(object):
 
-    res = net.solve(solver, emb, s=.25, num_reads=1000, verbose=verbose)
-    results = res.results()
-    results_not_broken = [x for x in results if not x['broken']]
-    print 'total: ', len(results), 'not broken: ', len(results_not_broken)
+    def __init__(self, solver, groups, links):
+        self._enemyTypes = {'riv'}
+        self.solver = solver
+        self.graphs = tg.create_graphs_by_date(groups, links)
+        self._compose_graphs()
+        self._make_groups_to_dwave_nodes()
+        self._make_embedding()
 
-    if print_res:
-        print 'results_not_broken:'
-        for r in results_not_broken:
-            print ' ', r
-    else:
-        print results_not_broken[0]
-    return results_not_broken[0]
+    def _compose_graphs(self):
+        comp_graph = nx.Graph()
+        graphs = []
+        for (date, grph) in self.graphs:
+            comp_graph = nx.compose(comp_graph, grph)
+            comp_graph.graph['date'] = date
+            graphs.append(comp_graph)
+        self.graphs = graphs
 
-def add_link(net, n1, n2, type):
-    enemyTypes = {'riv'}
-    if type in enemyTypes:
-        net.enemy(n1, n2)
-    else:
-        net.friend(n1, n2)
+    def _make_embedding(self):
+        # use the fully connected total graph to set the embedding
+        # from groups to dwave nodes
+        grph = self.graphs[-1]
+        net = self._create_network(grph)
+        self.embedding = social.Embedding(self.solver, net, verbose=0)
 
-def create_network(graph, nodes_to_indices=None):
+    def _add_link(self, net, n1, n2, type):
+        if type in self._enemyTypes:
+            net.enemy(n1, n2)
+        else:
+            net.friend(n1, n2)
 
-    nodes = graph.nodes(data=True)
-    if nodes_to_indices is None:
-        nodes_to_indices = {n[0]: i for (i,n) in enumerate(nodes)}
-    net = social.Network()
-    for edge in graph.edges(data=True):
-        n1 = nodes_to_indices[edge[0]]
-        n2 = nodes_to_indices[edge[1]]
-        type = edge[2]['type']
-        add_link(net, n1, n2, type)
-        #print 'add_link:', n1, n2, type, edge[0], edge[1]
-    return net, nodes_to_indices
+    def _make_groups_to_dwave_nodes(self):
+        # use the fully connected total graph to set the mapping
+        # from groups to dwave nodes
+        grph = self.graphs[-1]
+        nodes = grph.nodes(data=False)
+        self.groups_to_dwave_nodes = {n: i for (i, n) in enumerate(nodes)}
+
+    def _create_network(self, graph):
+        nodes = graph.nodes(data=True)
+        net = social.Network()
+        for edge in graph.edges(data=True):
+            n1 = self.groups_to_dwave_nodes[edge[0]]
+            n2 = self.groups_to_dwave_nodes[edge[1]]
+            type = edge[2]['type']
+            self._add_link(net, n1, n2, type)
+            #print 'add_link:', n1, n2, type, edge[0], edge[1]
+        return net
 
 
-def solve_graph(grph, date, print_res=False, pos=None, emb=None, nodes_to_indices=None, save_plot=False):
-    net, nodes_to_indices = create_network(grph, nodes_to_indices=nodes_to_indices)
-    if emb is None:
-        emb = social.Embedding(solver, net, verbose=0)
-    res = solve(net, emb, print_res=print_res)
+    def _solve(self, net, verbose=0, print_res=False):
+        print
+        print 'net.j:', net.j()
 
-    def set_node_weight(grph, res):
-        local_nodes = grph.nodes(data=True)
-        local_nodes_to_indices = {n[0]: i for (i, n) in enumerate(local_nodes)}
+        res = net.solve(self.solver, self.embedding, s=.25, num_reads=1000, verbose=verbose)
+        results = res.results()
+        results_not_broken = [x for x in results if not x['broken']]
+        print 'total: ', len(results), 'not broken: ', len(results_not_broken)
+
+        if print_res:
+            print 'results_not_broken:'
+            for r in results_not_broken:
+                print ' ', r
+        else:
+            print results_not_broken[0]
+        return results_not_broken[0]
+
+    def _set_node_weights(self, grph, res):
         spins = res['spins']
-        #print 'spins: ', len(spins)
+
+        # print 'spins: ', len(spins)
 
         def set_weight(n, s):
-            #print 'set_weight:', n, s
+            # print 'set_weight:', n, s
             if s == 3:
-                n[1]['weight'] = 0
+                n['weight'] = 0
             else:
-                n[1]['weight'] = s
+                n['weight'] = s
 
         for (n1, n2) in grph.edges():
-            i1 = nodes_to_indices[n1]
-            i2 = nodes_to_indices[n2]
-            li1 = local_nodes_to_indices[n1]
-            li2 = local_nodes_to_indices[n2]
-            #print 'i1,i2:', i1, i2, n1, n2, local_nodes[li1], local_nodes[li2]
+            i1 = self.groups_to_dwave_nodes[n1]
+            i2 = self.groups_to_dwave_nodes[n2]
             s1 = spins[i1]
             s2 = spins[i2]
-            set_weight(local_nodes[li1], s1)
-            set_weight(local_nodes[li2], s2)
+            set_weight(grph.node[n1], s1)
+            set_weight(grph.node[n2], s2)
 
-    set_node_weight(grph, res)
-    delta = res['delta']
-    pos = ptg.plot(date, grph, pos=pos, title='{} (delta = {})'.format(date, delta), save=save_plot)
-    return pos, emb, nodes_to_indices
 
-if not use_dwave:
-    conn = local.local_connection
-    solver = conn.get_solver("c4-sw_sample")
-else:
-    token = os.environ['DWAVE_TOKEN']
-    print token
-    os.environ['no_proxy'] = 'localhost'
-    #print os.environ
-    conn = remote.RemoteConnection('https://localhost:10443/sapi', token)
-    solver = conn.get_solver("DW2X")
+    def solve_graphs(self, verbose=0, print_res=False, save_plot=False):
 
-terrorFile = "syria.json"
+        for grph in self.graphs:
+            net = self._create_network(grph)
+            res = self._solve(net, verbose=verbose, print_res=print_res)
 
-groups, links = tg.extract_data_json(terrorFile)
-graphs = tg.create_graphs_by_date(groups, links)
+            self._set_node_weights(grph, res)
 
-date, grph = graphs[-1]
-#print 'edges:', grph.edges()
-pos, emb, nodes_to_indices = solve_graph(grph, date, print_res=True)
-#print nodes_to_indices
+            delta = res['delta']
+            grph.graph['delta'] = delta
 
-grph = nx.Graph()
-for (date, g) in graphs:
-    grph = nx.compose(grph, g)
-    #print
-    #print 'edges:', g.edges();
-    #print 'edges:', grph.edges()
-    solve_graph(grph, date, pos=pos, emb=emb, nodes_to_indices=nodes_to_indices, print_res=False, save_plot=save_plot)
+if __name__ == '__main__':
+
+    if not use_dwave:
+        conn = local.local_connection
+        solver = conn.get_solver("c4-sw_sample")
+    else:
+        token = os.environ['DWAVE_TOKEN']
+        print token
+        os.environ['no_proxy'] = 'localhost'
+        #print os.environ
+        conn = remote.RemoteConnection('https://localhost:10443/sapi', token)
+        solver = conn.get_solver("DW2X")
+
+    def plot(graphs):
+        # use the fully connected total graph to set the embedding
+        # from groups to dwave nodes
+        grph = graphs[-1]
+        pos = nx.spring_layout(grph, iterations=1000)
+
+        for grph in graphs:
+            delta = grph.graph['delta']
+            date = grph.graph['date']
+            pos = ptg.plot(date, grph, pos=pos, title='{} (delta = {})'.format(date, delta), save=save_plot)
+
+    terrorFile = "syria.json"
+
+    groups, links = tg.extract_data_json(terrorFile)
+
+    stanford_dwave = StanfordDwave(solver, groups, links)
+
+    stanford_dwave.solve_graphs()
+
+    plot(stanford_dwave.graphs)
