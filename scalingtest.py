6085b670a7d06324c3f1438c22b362263af7c6c0
John Ambrosiano <ambro@lanl.gov>
2017-04-22 14:37:38 -0600

    Scaling test code and cases completed.

--- scalingtest.py
+++ scalingtest.py
@@ -2,6 +2,7 @@ import dwave_sapi2.local as local
 import dwave_sapi2.remote as remote
 
 import os
+import sys
 
 import networkx as nx
 
@@ -17,9 +18,14 @@ import time
 if __name__=='__main__':
 
     use_dwave = True
-    nn = 50
-
+    nmin = 5
+    nmax = 50
     gtype = 'erdos'
+
+    ctype = 'classical'
+    if use_dwave:
+        ctype = 'dwave'
+    output = ctype+'-scaling-'+gtype+'.csv'
 
     # Use the D Wave machine or local solver.
     if not use_dwave:
@@ -34,48 +40,70 @@ if __name__=='__main__':
         conn = remote.RemoteConnection('https://localhost:10443/sapi', token)
         solver = conn.get_solver("DW2X")
 
+    # Don't use the profiler for timing. Too fine-grained, misleading.
     # Set up the profiler and enable.
-    #pr = cProfile.Profile()
-    #pr.enable()
+    # pr = cProfile.Profile()
+    # pr.enable()
 
-    # Make a random graph of 10 nodes.
-    g = sc.make_random_graph(nn,gtype)
-    ne = len(g.edges())
+    fout = open(output, 'wb')
+    fout.write('nnodes,tembedding,tsolve,tqpu\n')
+    try:
+        for nn in range(nmin,nmax+1):
+            # Make a random graph of  nodes.
+            g = sc.make_random_graph(nn,gtype)
+            ne = len(g.edges())
 
-    # Create a social network from the graph.
-    socnet = sc.make_social_network(g)
+            # Create a social network from the graph.
+            socnet = sc.make_social_network(g)
 
-    # Get an embedding for the graph.
-    start_time = time.time()
-    embedding = soc.Embedding(solver,socnet)
-    end_time = time.time()
-    print('Total embedding time = '+str(end_time-start_time)+' seconds.')
+            # Get an embedding for the graph.
+            start_time = time.time()
+            embedding = soc.Embedding(solver,socnet)
+            end_time = time.time()
+            tembedding = end_time-start_time
+            print('Total embedding time = '+str(tembedding)+' seconds.')
 
-    # Solve the embedded problem.
-    start_time = time.time()
-    solution = socnet.solve(solver,embedding)
-    end_time = time.time()
-    print('Total solution time = '+str(end_time-start_time) + ' seconds.')
-    if use_dwave==True:
-        qpu_access_time = solution._orig_results['timing']['qpu_access_time']
-        print('qpu_access_time = '+str(qpu_access_time*1.e-6)+' seconds.')
+            # Solve the embedded problem.
+            start_time = time.time()
+            solution = socnet.solve(solver,embedding)
+            end_time = time.time()
+            tsolve = end_time-start_time
+            print('Total solution time = '+str(tsolve) + ' seconds.')
+            qpuval = 'n/a'
+            if use_dwave==True:
+                qpu_access_time = solution._orig_results['timing']['qpu_access_time']
+                qpuval = str(qpu_access_time*1.e-6)
+                print('qpu_access_time = '+qpuval+' seconds.')
+            fout.write(str(nn)+','+str(tembedding)+','+str(tsolve)+','+qpuval+'\n')
 
-    # Stop the profiler and collect results.
-    #pr.disable()
-    # s = io.StringIO()
-    # sortby = 'cumulative'
-    # ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    # ps.print_stats()
-    # print(s.getvalue())
-    # ps = pstats.Stats(pr).strip_dirs().sort_stats('time')
-    # pr.create_stats()
-    # pr.print_stats()
+            # Stop the profiler and collect results.
+            # pr.disable()
+            # s = io.StringIO()
+            # sortby = 'cumulative'
+            # ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            # ps.print_stats()
+            # print(s.getvalue())
+            # ps = pstats.Stats(pr).strip_dirs().sort_stats('name')
+            # stats = ps.stats
+            # for k in stats.keys():
+            #     if ('embed_problem' in k)&('social.py' in k):
+            #         embeddingcpu = stats[k][3]
+            #     if ('solve' in k)&('social.py' in k):
+            #         solvecpu = stats[k][3]
+            # pr.create_stats()
+            # pr.print_stats()
+            # print('Problem embedding cpu = '+str(embeddingcpu))
+            # print('Problem solve cpu = '+ str(solvecpu))
 
 
-
-    sJs = solution.results()[0]['sJs:']
-    delta = 0.5*(float(ne) + sJs)
-
-    print('Solution found. Delta = '+str(delta))
+            # sJs = solution.results()[0]['sJs:']
+            # delta = 0.5*(float(ne) + sJs)
+            #
+            # print('Solution found. Delta = '+str(delta))
+    except:
+        fout.close()
+        sys.exit(1)
+    fout.close()
+    sys.exit(0)
 
 
